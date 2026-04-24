82663c1e954e6cfad44d2b08a7df83aa1afb908b
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-04-24 15:48:06 -0300

    feat(usuario): adiciona métodos Atualizar, ObterPorId e validação de email no repository

--- GerenciadorDeUsuarios.Application/Interfaces/IUsuarioRepository.cs
+++ GerenciadorDeUsuarios.Application/Interfaces/IUsuarioRepository.cs
@@ -5,5 +5,8 @@ namespace GerenciadorDeUsuarios.Application.Interfaces;
 public interface IUsuarioRepository
 {
     void Adicionar(Usuario usuario);
+    void Atualizar(Usuario usuario);
     bool ExistePorEmail(string email);
+    bool EmailJaUsadoPorOutroUsuario(string email, Guid id);
+    Usuario? ObterPorId(Guid id);
 }
