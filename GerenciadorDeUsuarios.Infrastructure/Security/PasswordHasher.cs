9c782f8b6ca7b4ad7807bd1b52e42d2a9f583e25
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-04-28 15:13:13 -0300

    feat(security): adiciona hash de senha com SHA256

---
+++ GerenciadorDeUsuarios.Infrastructure/Security/PasswordHasher.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using GerenciadorDeUsuarios.Application.Interfaces;
+
+namespace GerenciadorDeUsuarios.Infrastructure.Security;
+
+public class PasswordHasher : IPasswordHasher
+{
+    public string Hash(string senha)
+    {
+        using var sha256 = SHA256.Create();
+        var bytes = Encoding.UTF8.GetBytes(senha);
+        var hashBytes = sha256.ComputeHash(bytes);
+
+        return Convert.ToBase64String(hashBytes);
+
+
+    }
+}
