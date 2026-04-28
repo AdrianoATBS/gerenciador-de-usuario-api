1732f2208470c1f0d1cc486f665bf8f04369e660
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-04-28 15:14:30 -0300

    feat(db): adiciona AppDbContext com DbSet de Usuario

---
+++ GerenciadorDeUsuarios.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using GerenciadorDeUsuarios.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GerenciadorDeUsuarios.Infrastructure.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+    public DbSet<Usuario> Usuarios {  get; set; }
+}
