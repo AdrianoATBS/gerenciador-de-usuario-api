fda665e7b6df471c99e99bd496e43d2f9b438aa4
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-04-30 15:44:40 -0300

    feat(api): adiciona endpoints de usuários e configuração de DI

---
+++ GerenciadorDeUsuarios.API/Controller/UsuariosController.cs
@@ -0,0 +1,64 @@
+﻿using GerenciadorDeUsuarios.Application.UseCases.AlterarEmail;
+using GerenciadorDeUsuarios.Application.UseCases.AlterarNome;
+using GerenciadorDeUsuarios.Application.UseCases.CriarUsuario;
+using GerenciadorDeUsuarios.Application.UseCases.DesativarUsuario;
+using GerenciadorDeUsuarios.Application.UseCases.ReativarUsuario;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GerenciadorDeUsuarios.API.Controller;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsuariosController : ControllerBase
+{
+    private readonly CriarUsuarioUseCase _criarUsuario;
+    private readonly AlterarNomeUseCase _alterarNome;
+    private readonly AlterarEmailUseCase _alterarEmail;
+    private readonly DesativarUsuarioUseCase _desativarUsuario;
+    private readonly ReativarUsuarioUseCase _reativarUsuario;
+    public UsuariosController(CriarUsuarioUseCase criarUsuario
+        , AlterarNomeUseCase alterarNome, AlterarEmailUseCase alterarEmail,
+        DesativarUsuarioUseCase desativarUsuario, ReativarUsuarioUseCase
+        reativarUsuario)
+    {
+        _criarUsuario = criarUsuario;
+        _alterarNome = alterarNome;
+        _alterarEmail = alterarEmail;
+        _desativarUsuario = desativarUsuario;
+        _reativarUsuario = reativarUsuario;
+    }
+
+    [HttpPost]
+    public IActionResult CriarUsuario(CriarUsuarioRequest request)
+    {
+        _criarUsuario.Executar(request);
+        return Ok("Usuário criado com sucesso!");
+    }
+
+    [HttpPut("nome")]
+    public IActionResult AlterarNome(AlterarNomeRequest request)
+    {
+        _alterarNome.Executar(request);
+        return Ok("Nome do usuário alterado com sucesso!");
+    }
+
+    [HttpPut("email")]
+    public IActionResult AlterarEmail(AlterarEmailRequest request)
+    {
+        _alterarEmail.Executar(request);
+        return Ok("Email do usuário alterado com sucesso!");
+    }
+    [HttpPut("desativar")]
+    public IActionResult DesativarUsuario(DesativarUsuarioRequest request)
+    {
+        _desativarUsuario.Executar(request);
+        return Ok("Usuário desativado com sucesso!");
+    }
+    [HttpPut("reativar")]
+    public IActionResult ReativarUsuario(ReativarUsuarioRequest request)
+    {
+        _reativarUsuario.Executar(request);
+        return Ok("Usuário reativado com sucesso!");
+
+    }
+}
--- GerenciadorDeUsuarios.API/Program.cs
+++ GerenciadorDeUsuarios.API/Program.cs
@@ -1,4 +1,12 @@
+using GerenciadorDeUsuarios.Application.Interfaces;
+using GerenciadorDeUsuarios.Application.UseCases.AlterarEmail;
+using GerenciadorDeUsuarios.Application.UseCases.AlterarNome;
+using GerenciadorDeUsuarios.Application.UseCases.CriarUsuario;
+using GerenciadorDeUsuarios.Application.UseCases.DesativarUsuario;
+using GerenciadorDeUsuarios.Application.UseCases.ReativarUsuario;
 using GerenciadorDeUsuarios.Infrastructure.Data;
+using GerenciadorDeUsuarios.Infrastructure.Repositories;
+using GerenciadorDeUsuarios.Infrastructure.Security;
 using Microsoft.EntityFrameworkCore;
 
 
@@ -8,6 +16,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<CriarUsuarioUseCase>();
+builder.Services.AddScoped<AlterarEmailUseCase>();
+builder.Services.AddScoped<AlterarNomeUseCase>();
+builder.Services.AddScoped<DesativarUsuarioUseCase>();
+builder.Services.AddScoped<ReativarUsuarioUseCase>();
+
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
