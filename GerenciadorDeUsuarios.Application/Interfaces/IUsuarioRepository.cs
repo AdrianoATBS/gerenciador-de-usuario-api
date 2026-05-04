67f925b00247c7c133dfd8ac3df1365490f1d729
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-05-04 15:11:08 -0300

    fix(usuario): adicionar método deletar ao repositorio

--- GerenciadorDeUsuarios.Application/Interfaces/IUsuarioRepository.cs
+++ GerenciadorDeUsuarios.Application/Interfaces/IUsuarioRepository.cs
@@ -6,7 +6,9 @@ public interface IUsuarioRepository
 {
     void Adicionar(Usuario usuario);
     void Atualizar(Usuario usuario);
+    void Deletar(Usuario usuario);
     bool ExistePorEmail(string email);
     bool EmailJaUsadoPorOutroUsuario(string email, Guid id);
     Usuario? ObterPorId(Guid id);
+
 }
