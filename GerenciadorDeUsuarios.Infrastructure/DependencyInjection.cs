bedd9dbcdcea4042b0fe09e484e3107a977cbeb5
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-05-21 16:18:31 -0300

    refactor(infra): extrai configurações de jwt e swagger

---
+++ GerenciadorDeUsuarios.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using GerenciadorDeUsuarios.Application.Interfaces;
+using GerenciadorDeUsuarios.Infrastructure.Repositories;
+using GerenciadorDeUsuarios.Infrastructure.Security;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GerenciadorDeUsuarios.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure
+        (this IServiceCollection services)
+    {
+        services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+        services.AddScoped<IPasswordHasher, PasswordHasher>();
+        services.AddScoped<ITokenService, JwtTokenService>();
+        return services;
+    }
+
+}
---
+++ GerenciadorDeUsuarios.Infrastructure/Extensions/JwtExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace GerenciadorDeUsuarios.Infrastructure.Extensions;
+
+public static class JwtExtensions
+{
+    public static IServiceCollection AddJwtAuthentication(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var chaveSecreta = configuration["JwtSettings:SecretKey"];
+        var chave = Encoding.ASCII.GetBytes(chaveSecreta!);
+
+        services.AddAuthentication(x =>
+         {
+            x.DefaultAuthenticateScheme = 
+                JwtBearerDefaults.AuthenticationScheme;
+            x.DefaultChallengeScheme = 
+                JwtBearerDefaults.AuthenticationScheme;
+         })
+
+        .AddJwtBearer(x =>
+        {
+            x.RequireHttpsMetadata = false;
+            x.SaveToken = true;
+            x.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(chave),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ClockSkew = TimeSpan.Zero
+            };
+        });
+        return services;
+    }
+}
