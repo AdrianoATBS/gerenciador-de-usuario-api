c5ae96d1c771dda91c138a3add165c7100575650
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-05-21 16:18:03 -0300

    refactor(application): centraliza injeções de dependência

---
+++ GerenciadorDeUsuarios.Application/DependencyInjection.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+using GerenciadorDeUsuarios.Application.UseCases.AlterarEmail;
+using GerenciadorDeUsuarios.Application.UseCases.AlterarNome;
+using GerenciadorDeUsuarios.Application.UseCases.CriarUsuario;
+using GerenciadorDeUsuarios.Application.UseCases.DeletarUsuario;
+using GerenciadorDeUsuarios.Application.UseCases.DesativarUsuario;
+using GerenciadorDeUsuarios.Application.UseCases.LoginUsuario;
+using GerenciadorDeUsuarios.Application.UseCases.ReativarUsuario;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GerenciadorDeUsuarios.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(
+        this IServiceCollection services)
+    {
+        services.AddScoped<CriarUsuarioUseCase>();
+        services.AddScoped<AlterarEmailUseCase>();
+        services.AddScoped<AlterarNomeUseCase>();
+        services.AddScoped<DesativarUsuarioUseCase>();
+        services.AddScoped<ReativarUsuarioUseCase>();
+        services.AddScoped<DeletarUsuarioUseCase>();
+        services.AddScoped<LoginUsuarioUseCase>();
+        return services;
+    }
+
+  
+
+}
