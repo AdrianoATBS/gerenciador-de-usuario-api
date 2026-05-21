d4d85f459477f3788dfcff642bac8b106f8a5396
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-05-21 16:18:49 -0300

    refactor(api): simplifica configuração do Program.cs

--- GerenciadorDeUsuarios.API/Program.cs
+++ GerenciadorDeUsuarios.API/Program.cs
@@ -1,102 +1,27 @@
-using GerenciadorDeUsuarios.Application.Interfaces;
-using GerenciadorDeUsuarios.Application.UseCases.AlterarEmail;
-using GerenciadorDeUsuarios.Application.UseCases.AlterarNome;
-using GerenciadorDeUsuarios.Application.UseCases.CriarUsuario;
-using GerenciadorDeUsuarios.Application.UseCases.DeletarUsuario;
-using GerenciadorDeUsuarios.Application.UseCases.DesativarUsuario;
-using GerenciadorDeUsuarios.Application.UseCases.LoginUsuario;
-using GerenciadorDeUsuarios.Application.UseCases.ReativarUsuario;
 using GerenciadorDeUsuarios.Infrastructure.Data;
-using GerenciadorDeUsuarios.Infrastructure.Repositories;
-using GerenciadorDeUsuarios.Infrastructure.Security;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
-using System.Text;
-
+using GerenciadorDeUsuarios.Infrastructure;
+using GerenciadorDeUsuarios.Application;
+using GerenciadorDeUsuarios.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "Gerenciador de Usuariios" +
-        "API",
-        Version = "v1"
-    });
-    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = "Insira o token JWT no formato: Bearer {seu token}"
-    });
 
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            Array.Empty<string>()
-        }
-    });
-    
-});
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure();
+builder.Services.AddSwaggerDocumentation();
+builder.Services.AddJwtAuthentication(builder.Configuration);
 
-builder.Services.AddScoped<CriarUsuarioUseCase>();
-builder.Services.AddScoped<AlterarEmailUseCase>();
-builder.Services.AddScoped<AlterarNomeUseCase>();
-builder.Services.AddScoped<DesativarUsuarioUseCase>();
-builder.Services.AddScoped<ReativarUsuarioUseCase>();
-builder.Services.AddScoped<DeletarUsuarioUseCase>();
-builder.Services.AddScoped<LoginUsuarioUseCase>();
-
-builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
-builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
-builder.Services.AddScoped<ITokenService, JwtTokenService>();
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var connectionString = builder.Configuration.GetConnectionString
+    ("DefaultConnection");
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 options.UseSqlServer(connectionString));
-var  chaveSecreta = builder.Configuration["JwtSettings:SecretKey"];
-var chave = Encoding.ASCII.GetBytes(chaveSecreta);
-
-builder.Services.AddAuthentication(x =>
-{
-    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-
-.AddJwtBearer(x =>
-{
-    x.RequireHttpsMetadata = false;
-    x.SaveToken = true;
-    x.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(chave),
-        ValidateIssuer = false,
-        ValidateAudience = false,
-        ClockSkew = TimeSpan.Zero
-    };
-});
 
 var app = builder.Build();
-
 
 if (app.Environment.IsDevelopment())
 {
